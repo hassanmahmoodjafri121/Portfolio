3495bd148f7498f1204b0b534b62966e30d6c493
hassanmahmoodjafri121 <100502783+Sufianmahmood@users.noreply.github.com>
2025-08-21 16:48:33 +0530

    portfolio

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -4,10 +4,10 @@ import Header from './components/Header';
 import Home from './components/Hero';
 import About from './components/About';
 import Skills from './components/Skills';
-import Experience from './components/Experience'; // ✅ Imported here
+import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from './components/Footer';
+import Footer from "./components/Footer";
 import './index.css';
 
 function App() {
