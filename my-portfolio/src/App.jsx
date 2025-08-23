63a37324dbe8cae39644314e14c1db9e357c59ee
hassanmahmoodjafri121 <97827696+hassanmahmoodjafri121@users.noreply.github.com>
2025-08-23 13:58:55 +0000

    portfolio

--- my-portfolio/src/App.jsx
+++ my-portfolio/src/App.jsx
@@ -7,7 +7,7 @@ import Skills from './components/Skills';
 import Experience from './components/Experience';
 import Projects from './components/Projects';
 import Contact from './components/Contact';
-import Footer from "./components/Footer";
+import Footer from "./components/footer";
 import './index.css';
 
 function App() {
